9f9b27e53d1165a842dced7fec7144b9179d5774
satyapal <satyapal.info@gmail.com>
2018-08-02 22:42:10 +0530

    changes for uat setup

--- referral/kafka_user_update.py
+++ referral/kafka_user_update.py
@@ -20,39 +20,41 @@ import pymysql.cursors
 #     "DB_NAME": "FabHotels"
 # }
 
-# db_read_config = {
-#     "DB_HOST": "localhost",
-#     "DB_USER": "fabDev",
-#     "DB_PASS": "Fab@1962",
-#     "DB_NAME": "FabHotels"
-# }
-
-# db_write_config = {
-#     "DB_HOST": "localhost",
-#     "DB_USER": "fabDev",
-#     "DB_PASS": "Fab@1962",
-#     "DB_NAME": "FabHotels"
-# }
-
 db_read_config = {
-    "DB_HOST": "fabuser-microservice-read.cwwl28odsw8p.ap-south-1.rds.amazonaws.com",
-    "DB_USER": "refferral_read",
-    "DB_PASS": "mSq6Tr9ZYNw6ryqB",
-    "DB_NAME": "refferral_microservice"
+    "DB_HOST": "localhost",
+    "DB_USER": "root",
+    "DB_PASS": "3p7G(kyJ?yN)~22X",
+    "DB_NAME": "referral_microservice"
 }
 
 db_write_config = {
-    "DB_HOST": "fabuser-microservice.cwwl28odsw8p.ap-south-1.rds.amazonaws.com",
-    "DB_USER": "refferral_write",
-    "DB_PASS": "ushRtSTvEg5nytDJ",
-    "DB_NAME": "refferral_microservice"
+    "DB_HOST": "localhost",
+    "DB_USER": "root",
+    "DB_PASS": "3p7G(kyJ?yN)~22X",
+    "DB_NAME": "referral_microservice"
 }
+
+# db_read_config = {
+#     "DB_HOST": "fabuser-microservice-read.cwwl28odsw8p.ap-south-1.rds.amazonaws.com",
+#     "DB_USER": "refferral_read",
+#     "DB_PASS": "mSq6Tr9ZYNw6ryqB",
+#     "DB_NAME": "refferral_microservice"
+# }
+
+# db_write_config = {
+#     "DB_HOST": "fabuser-microservice.cwwl28odsw8p.ap-south-1.rds.amazonaws.com",
+#     "DB_USER": "refferral_write",
+#     "DB_PASS": "ushRtSTvEg5nytDJ",
+#     "DB_NAME": "refferral_microservice"
+# }
 
 # POINTS_SERVICE_URL = 'http://13.127.243.15:8080'
 
 # POINTS_SERVICE_URL = 'http://172.31.7.216:8080'
 
-POINTS_SERVICE_URL = 'http://internal-points-load-balancer-1148785792.ap-south-1.elb.amazonaws.com:8020'
+# POINTS_SERVICE_URL = 'http://internal-points-load-balancer-1148785792.ap-south-1.elb.amazonaws.com:8020'
+
+POINTS_SERVICE_URL = 'http://localhost:8070'
 
 POINTS_PLAN_OBJECT = {}
 
